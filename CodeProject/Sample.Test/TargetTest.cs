0ce5fa49e9c6af482b79c788bdd230570b3e5948
Anil <anil.krishnamaneni@wonga.com>
2015-09-23 16:34:35 +0100

    Fixing Nunit

--- CodeProject/Sample.Test/TargetTest.cs
+++ CodeProject/Sample.Test/TargetTest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sample.Test
+﻿namespace Sample.Test
 {
     using NUnit.Framework;
 
